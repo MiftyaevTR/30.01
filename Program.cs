f8a9b92dd4fd8eeef5245c87b44c39df789a713f
Timur <t926661227@yandex.ru>
2023-02-01 13:37:36 +0300

    Final ex

--- Program.cs
+++ Program.cs
@@ -76,9 +76,20 @@ int N =Convert.ToInt32(Console.ReadLine());
 int result = GetSummBetween2Digit(M,N);
 Console.WriteLine(result);
 */
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+/*
+int Akkerman (int m,int n)
+{
+    if (m==0) return n+1;
+    else 
+    {
+        if (m!=0 && n==0) return Akkerman(m-1,1);
+        else return Akkerman(m-1, Akkerman(m,n-1));
+    }
+}
 
-
-
+Console.Write(Akkerman(3,2));
+*/
 
 
 
